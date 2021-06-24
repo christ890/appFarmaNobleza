6f4321a64791197ca874417aacfc5ab6f524efd4
Christopher Quiroz <christ890.098@gmail.com>
2021-06-23 20:01:04 -0500

    Initial Commit

---
+++ appFarmaNobleza/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appFarmaNobleza
+//
+//  Created by DARK NOISE on 23/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
